c724c27f09dbae207afde0c0fd5a859b39384c0c
fmuinos <ferran.muinos@irbbarcelona.org>
2021-05-20 13:16:52 +0200

    ADD: figure 2 needleplot and SHAP clustering

---
+++ Figure2/scripts/fetch_data_needle.py
@@ -0,0 +1,76 @@
+import sys
+import os
+
+import seaborn as sns
+sns.set_style("whitegrid", {'axes.grid' : False})
+import pandas as pd
+import numpy as np
+
+import conf
+import oncotree
+
+
+tree = oncotree.Oncotree()
+
+
+def get_mutations():
+
+    obs_mut = pd.read_csv(os.path.join(conf.output_boostdm, 'discovery', 'mutations.tsv'), sep='\t')
+    obs_mut.rename(columns={'mut': 'alt'}, inplace=True)
+    obs_mut['chr'] = obs_mut['chr'].astype(str)
+    obs_mut['pos'] = obs_mut['pos'].astype(int)
+    return obs_mut
+
+
+def create_observed_dataset(gene, ttype, obs_mut):
+
+    sat_pred = pd.read_csv(os.path.join(conf.output_boostdm,
+                                        'saturation',
+                                        'prediction',
+                                        f'{gene}.{ttype}.prediction.tsv.gz'),
+                           sep='\t')
+    sat_pred['chr'] = sat_pred['chr'].astype(str)
+    sat_pred['pos'] = sat_pred['pos'].astype(int)
+    cohorts_under_ttype = list(zip(*tree.get_cohorts(ttype)))[0]
+    obs_mut = obs_mut[(obs_mut['gene'] == gene) & (obs_mut['COHORT'].isin(cohorts_under_ttype))]
+    df = pd.merge(obs_mut, sat_pred, on=['chr', 'pos', 'alt', 'gene', 'aachange'], how='inner')
+    return df
+
+
+def get_position(row):
+
+    try:
+        v = int("".join(row["aachange"][1:-1]))
+        return v
+    except:
+        return -1
+
+
+def get_plot_data(data):
+
+    df_stats = pd.read_csv(conf.cohorts_path, sep="\t")
+    df_stats.rename(columns={"CANCER_TYPE": "cancer_type"},inplace=True)
+
+    data = data.merge(df_stats[["COHORT", "cancer_type"]])
+
+    data["Protein_position"] = data.apply(lambda row: get_position(row), axis=1)
+    data["AA"] = data.apply(lambda row: row["aachange"][0], axis=1)
+    data['ID'] = data.apply(lambda x: '{}_{}'.format(x['pos'], x['alt']), axis=1)
+
+    score_values = data['boostDM_score'].tolist()
+    count_driver_unique = len(set(data[data['boostDM_class']]['ID']))
+    count_driver = len(data[data['boostDM_class']]['ID'])
+    count_total = len(data['ID'])
+    count_total_unique = len(set(data['ID']))
+
+    df_counts = data.groupby(["pos", "ENSEMBL_TRANSCRIPT", "cancer_type"],
+                             as_index=False).agg({"sampleID": "count"})
+    df_counts.rename(columns={"sampleID": "number_observed_muts"}, inplace=True)
+    data = data.merge(df_counts, how='left')
+    data = data.groupby(["pos", "AA", "Protein_position", "gene", "ENSEMBL_TRANSCRIPT",
+                         "cancer_type", "number_observed_muts"], as_index=False).agg(
+        {"boostDM_score": np.nanmax, "boostDM_class": np.any})
+
+    data.rename(columns={"cancer_type": "cancer_type_annotations", }, inplace=True)
+
+    return data, count_driver, count_driver_unique, count_total, count_total_unique, score_values
---
+++ Figure2/scripts/retrieve.py
@@ -0,0 +1,88 @@
+# retrieve predictions and explanations
+
+import os
+import sys
+sys.path.append('..')
+import glob
+import tqdm
+import pickle
+import gzip
+
+import pandas as pd
+
+import conf
+import oncotree
+
+
+tree = oncotree.Oncotree()
+
+create_datasets_folder = os.path.join(conf.output_boostdm, 'create_datasets')
+eval_folder = os.path.join(conf.output_boostdm, 'evaluation')
+saturation_prediction_folder = os.path.join(conf.output_boostdm, 'saturation', 'prediction')
+mutations_path = os.path.join(conf.output_boostdm, 'discovery', 'mutations.tsv')
+
+ttypes = tree.get_ttypes('CANCER')
+
+# mapping MoA colors to each (gene, ttype)
+dg = pd.read_csv(conf.drivers_path, sep='\t')
+d = dict(zip(zip(dg.SYMBOL, dg.CANCER_TYPE), map(lambda x: conf.dict_colors_role.get(x, '#808080'), dg.ROLE)))
+d_gene = dict(zip(dg.SYMBOL, map(lambda x: conf.dict_colors_role.get(x, '#808080'), dg.ROLE)))
+
+# mapping complexity to each (gene, ttype)
+complex_df = pd.read_csv(conf.complexity_path, sep='\t')
+complex_dict = dict(zip(zip(complex_df.gene, complex_df.ttype), complex_df.linear_complexity))
+
+# model selection dict
+with gzip.open(os.path.join(conf.output_boostdm, 'model_selection', 'eval_data.pickle.gz'), 'rb') as f:
+    model_selection_dict = pickle.load(f)
+
+
+def get_shaps(observed_mutations, gene=None, ttype=None):
+
+    dg = observed_mutations
+    if ttype is not None:
+        cohorts = list(zip(*tree.get_cohorts(ttype)))[0]
+        dg = dg[dg['COHORT'].isin(cohorts)].copy()
+    if gene is not None:
+        dg = dg[dg['gene'] == gene]
+    dg.rename(columns={'mut': 'alt'}, inplace=True)
+
+    data = []
+
+    for fn in tqdm.tqdm(glob.glob(os.path.join(saturation_prediction_folder, '*.*.prediction.tsv.gz'))):
+
+        g, tt = tuple((os.path.basename(fn)).split('.')[:2])
+        cohorts = list(zip(*tree.get_cohorts(tt)))[0]
+        dh = dg[dg['COHORT'].isin(cohorts)]
+        dh['ttype'] = tt
+
+        df_pred = pd.read_csv(fn, sep='\t')
+        df_pred = df_pred[(df_pred['boostDM_score'] >= 0.5)]
+        df_pred['moa'] = d.get((g, tt), d_gene.get((g, tt), '#808080'))
+        df_pred['linear_complexity'] = df_pred.apply(lambda r: complex_dict.get((r['selected_model_gene'],
+                                                                                 r['selected_model_ttype']), 0.),
+                                                     axis=1)
+        df_pred['ttype'] = tt
+        df_pred['gene'] = g
+        df_pred['aachange'].fillna(".", inplace=True)
+        df_pred = df_pred[['chr', 'pos', 'alt'] +
+                          conf.features +
+                          ['moa', 'linear_complexity', 'aachange'] +
+                          ['gene', 'ttype']]
+
+        # merge
+        dh['chr'] = dh['chr'].astype(str)
+        df_pred['chr'] = df_pred['chr'].astype(str)
+        df_data = pd.merge(dh, df_pred, on=['chr', 'pos', 'alt', 'gene', 'ttype', 'aachange'])
+
+        data.append(df_data)
+
+    df = pd.concat(data, axis=0)
+    df.drop_duplicates(['chr', 'pos', 'alt', 'gene', 'ttype', 'aachange'], keep='first', inplace=True)
+    return df
+
+
+if __name__ == '__main__':
+    pass
+
+
