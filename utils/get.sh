0e34c26ab41229dff69a514bb165d58edc6646b9
durvasa <ferranmuinos@gmail.org>
2021-07-12 12:41:31 +0200

    FIX: script to retrieve source data

--- utils/get.sh
+++ utils/get.sh
@@ -1,17 +1,13 @@
-# source get.sh <target data folder>
+# Script to retrieve the source data from zenodo
+# $ pip install zenodo_get
+# $ source get.sh <your-folder>
 
 DOI="10.5281/zenodo.4813082"
 
-# define the env variable SOURCE_DATA in export.sh
+# define an env variable SOURCE_DATA in export.sh
 
 SOURCE_DATA=$1
 
 bash -c "mkdir -p ${SOURCE_DATA} && export SOURCE_DATA=${SOURCE_DATA}"
-
-# source export.sh
-
-echo "source data: ${SOURCE_DATA}"
-
-# pip install zenodo_get
 
 zenodo_get ${DOI} -o ${SOURCE_DATA}
