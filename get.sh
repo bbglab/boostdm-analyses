64eb2be017ae799d3d4e333f7eec1b94141d27c9
durvasa <ferranmuinos@gmail.com>
2021-07-21 00:46:21 +0200

    ADJUST: how to download source-data from zenodo

--- get.sh
+++ get.sh
@@ -1,13 +1,13 @@
 # Script to retrieve the source data from zenodo
 # $ pip install zenodo_get
-# $ source get.sh <your-folder>
+# $ bash get.sh <your-folder>
 
 DOI="10.5281/zenodo.4813082"
 
 # define an env variable SOURCE_DATA in export.sh
 
-SOURCE_DATA=$1
+PATH_SOURCE_DATA=$PWD/source-data
 
-bash -c "mkdir -p ${SOURCE_DATA} && export SOURCE_DATA=${SOURCE_DATA}"
+mkdir -p ${PATH_SOURCE_DATA}
 
-zenodo_get ${DOI} -o ${SOURCE_DATA}
+zenodo_get ${DOI} -o ${PATH_SOURCE_DATA}
